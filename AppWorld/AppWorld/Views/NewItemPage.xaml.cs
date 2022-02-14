92e0f001d8cca92202f9cb14a2b0093256553d22
Дмитрий <64152554+56vdv56@users.noreply.github.com>
2022-02-14 09:56:14 +0500

    Добавьте файлы проекта.

---
+++ AppWorld/AppWorld/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppWorld.Models;
+using AppWorld.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppWorld.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
